878bf5902fc12644ee1240e6e24e5a5749b58eb3
bvanchuhov <bodya.van@gmail.com>
2016-05-09 18:23:10 +0300

    server-bvan: create LinkUserToConversationServlet template

---
+++ server-bvan/src/main/java/com/bvan/chatee/presentation/LinkUserToConversationServlet.java
@@ -0,0 +1,11 @@
+package com.bvan.chatee.presentation;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * @author bvanchuhov
+ */
+public class LinkUserToConversationServlet extends HttpServlet {
+
+    //TODO: please, implement this functionality. i love you, darling)
+}
