2b1d51130f4908af800e89251617cbe5e4e590c9
bvanchuhov <bodya.van@gmail.com>
2016-05-15 01:42:38 +0300

    server-bvan: fix hello servlet's url pattern

--- server-bvan/src/main/java/com/bvan/chatee/presentation/test/HelloServlet.java
+++ server-bvan/src/main/java/com/bvan/chatee/presentation/test/HelloServlet.java
@@ -10,11 +10,11 @@ import java.io.IOException;
 /**
  * @author bvanchuhov
  */
-@WebServlet(urlPatterns = "hello")
+@WebServlet(urlPatterns = "/hello")
 public class HelloServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.sendRedirect("/WEB-INF/Hello.jsp");
+        req.getRequestDispatcher("/WEB-INF/Hello.jsp").forward(req, resp);
     }
 }
